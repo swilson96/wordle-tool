2bf8532dbcaf3c903faea37a3d0bd29ff1aac1ad
Simon Wilson <simon.wilson@hudl.com>
2022-06-04 22:52:30 +0100

    colours

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface ITileProps {
   letter: string;
 }
 
+type TileState = 'miss' | 'hit' | 'close';
+
 function Tile({ letter }: ITileProps) {
+  const [state, setState] = useState<TileState>('miss');
+  const handleClick = () => letter && setState(prev => prev == 'miss' ? 'hit' : prev == 'hit' ? 'close' : 'miss');
   return (
-    <div className="tile">
+    <div className={'tile ' + (letter ? state : 'empty' )} onClick={() => handleClick()}>
       {letter}
     </div>
   );
